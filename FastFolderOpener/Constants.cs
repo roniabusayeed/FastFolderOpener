46582ffc50a59f49aebd9751bcd14b5b3f3bf416
roniabusayeed <roni.abusayeed@gmail.com>
2022-09-07 20:47:24 +0600

    Add project files.

---
+++ FastFolderOpener/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFolderOpener
+{
+    internal class Constants
+    {
+        public const string APPLICATION_TITLE = "Fast Folder Opener";
+    }
+}
